73bd90312390796270a88e8364be52294b0f5d21
d0dders <dodders@outlook.com>
2020-10-26 19:16:46 +0000

    Update guessing_game.py
    Added High Score tracker and added some line spacing for comfort

--- guessing_game.py
+++ guessing_game.py
@@ -13,30 +13,19 @@ import random
 
 
 def start_game():
-    """Psuedo-code Hints
-    
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
-    ( You can add more features/enhancements if you'd like to. )
-    """
     # write your code inside this function.
     keep_playing = True
+    current_high_score = None
     while keep_playing:
         attempts = 0
         guess = -1
-        print("Welcome to the number guessing game")
+        print("Welcome to the number guessing game!!")
+        print("-------------------------------------\n")
+        if current_high_score != None:
+            print(f'The current HIGH SCORE is {current_high_score}\n')
         random_number = (random.randint(1,10))
-        print(random_number)
+        #For debugging purposes
+        #print(random_number)
         while guess != random_number:
             try:
                 try:
@@ -53,10 +42,14 @@ def start_game():
                     print(f"The number I'm thinking of is HIGHER than {guess}")
                 elif guess > random_number:
                     print(f"The number I'm thinking of is LOWER than {guess}")
-        print(f'Got it!! You guessed the right number! It took {attempts} attempts!')
-        
+        print(f'\nGot it!! You guessed the right number! It took {attempts} attempts!\n')
+        if current_high_score == None:
+            current_high_score = attempts
+        if attempts < current_high_score:
+            current_high_score = attempts
+        #Any answer that is not 'y' considered a No
         keep_playing = input("Would you like to play again? (y)es/(n)o  ").lower() == 'y'
-
+        print('\n')
 
 # Kick off the program by calling the start_game function.
 start_game()
