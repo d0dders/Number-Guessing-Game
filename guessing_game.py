a7225996e5bc084408566ccdf4e0e459ee546ead
d0dders <dodders@outlook.com>
2020-10-26 18:49:38 +0000

    Update guessing_game.py
    Improved exception handling and added abiity to replay game

--- guessing_game.py
+++ guessing_game.py
@@ -30,29 +30,32 @@ def start_game():
     ( You can add more features/enhancements if you'd like to. )
     """
     # write your code inside this function.
-    attempts = 0
-    guess = -1
-    print("Welcome to the number guessing game")
-    random_number = (random.randint(1,10))
-    print(random_number)
-    while guess != random_number:
-        try:
+    keep_playing = True
+    while keep_playing:
+        attempts = 0
+        guess = -1
+        print("Welcome to the number guessing game")
+        random_number = (random.randint(1,10))
+        print(random_number)
+        while guess != random_number:
             try:
-                guess = int(input("Guess what number I'm thinking of. It's between 1-10\n"))
+                try:
+                    guess = int(input("Guess what number I'm thinking of. It's between 1-10\n"))
+                except ValueError as err:
+                    raise ValueError("Try guessing with an actual number next time!")
+                if guess < 1 or guess > 10:
+                    raise ValueError("The number should be in range! Remember, it's between 1-10")
             except ValueError as err:
-                raise ValueError("Try guessing with an actual number next time!")
-            if guess < 1 or guess > 10:
-                raise ValueError("The number should be in range! Remember, it's between 1-10")
-        except ValueError as err:
-            print(err)
-        else:
-            attempts += 1
-            if guess < random_number:
-                print(f"The number I'm thinking of is HIGHER than {guess}")
-            elif guess > random_number:
-                print(f"The number I'm thinking of is LOWER than {guess}")
-    print(f'Got it!! You guessed the right number! It took {attempts} attempts!')
-    print("The games is ending, thanks for playing!")
+                print(err)
+            else:
+                attempts += 1
+                if guess < random_number:
+                    print(f"The number I'm thinking of is HIGHER than {guess}")
+                elif guess > random_number:
+                    print(f"The number I'm thinking of is LOWER than {guess}")
+        print(f'Got it!! You guessed the right number! It took {attempts} attempts!')
+        
+        keep_playing = input("Would you like to play again? (y)es/(n)o  ").lower() == 'y'
 
 
 # Kick off the program by calling the start_game function.
